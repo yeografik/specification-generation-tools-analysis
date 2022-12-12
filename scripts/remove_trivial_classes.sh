1e8b64fe08b8bdaea0962194c16c95d72386d3f6
yeografik <nachogonz2702@gmail.com>
2022-12-12 18:30:59 -0300

    Added more classes to script

--- scripts/remove_trivial_classes.sh
+++ scripts/remove_trivial_classes.sh
@@ -2,3 +2,8 @@
 
 sed -i '/java.lang.Object/d' $1
 sed -i '/java.lang.Integer/d' $1
+sed -i '/java.lang.Boolean/d' $1
+sed -i '/java.util.HashMap/d' $1
+sed -i '/java.util.ArrayList/d' $1
+sed -i '/java.util.HashSet/d' $1
+sed -i '/java.util.Set/d' $1
