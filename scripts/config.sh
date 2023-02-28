e450ec3906584fc3ca3dfd6a4fbe35efa1008d65
yeografik <nachogonz2702@gmail.com>
2023-02-28 18:14:15 -0300

    Changed some environment variables and added daikon jar to libs

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #project_root=$(pwd)
-export DAIKONDIR="libs/daikon-5.8.16"
+export DAIKON_JAR="libs/daikon.jar"
 export EVOSUITE_JAR="libs/evosuite-1.0.6.jar"
 export RANDOOP_JAR="libs/randoop-all-4.3.1.jar"
+export LITERALS="resources/literals.txt"
