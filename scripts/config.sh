4f1bb3b0bc3aa735fa23e3e146d07dd1703a86eb
yeografik <nachogonz2702@gmail.com>
2023-03-06 23:29:10 -0300

    Moved colors to config script

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+#colors
+export RED=$(tput setaf 1)
+export GREEN=$(tput setaf 2)
+export YELLOW=$(tput setaf 3)
+export NORMAL=$(tput sgr0)
+
 #project_root=$(pwd)
-export DAIKON_JAR="libs/daikon.jar"
+export DAIKONDIR="libs/daikon.jar"
 export EVOSUITE_JAR="libs/evosuite-1.0.6.jar"
 export RANDOOP_JAR="libs/randoop-all-4.3.1.jar"
 export LITERALS="resources/literals.txt"
