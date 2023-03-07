89836f66386e155806fe0fb8e6ca447e33b58eb0
yeografik <nachogonz2702@gmail.com>
2023-03-06 23:30:43 -0300

    Removed daikon, now its a dependence

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,7 +7,6 @@ export YELLOW=$(tput setaf 3)
 export NORMAL=$(tput sgr0)
 
 #project_root=$(pwd)
-export DAIKONDIR="libs/daikon.jar"
 export EVOSUITE_JAR="libs/evosuite-1.0.6.jar"
 export RANDOOP_JAR="libs/randoop-all-4.3.1.jar"
 export LITERALS="resources/literals.txt"
