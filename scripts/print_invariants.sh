34cf3801e2b866ba66e2dc41882c27fd645c81f9
Nacho Gonzalez <nachogonz2702@gmail.com>
2023-04-21 10:57:50 -0300

    Now evosuite tests driver is created like randoop driver

--- scripts/print_invariants.sh
+++ scripts/print_invariants.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #$1 invariants path, $2 full class package, $3 objective method, $4 output dir
-#$1 package, $2 class name, $3 name of class that contains tests, $4 objective method
 
 java -cp $DAIKONDIR/daikon.jar daikon.PrintInvariants $1 --ppt-select-pattern="$2:::OBJECT|$2.$3" > $4
 
-#provisory usage print_invariants.sh RegressionTestDriver.inv.gz DataStructures.StackAr push invariantes.txt
+#example usage ./scripts/print_invariants.sh RegressionTestDriver.inv.gz DataStructures.StackAr push invariantes.txt
