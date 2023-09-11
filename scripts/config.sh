170b21462ecad29a44098b065641be966d062b0f
Nacho Gonzalez <nachogonz2702@gmail.com>
2023-09-11 12:00:03 -0300

    Now evosuite and randoop time execution are configurable through config script

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,9 +6,13 @@ export GREEN=$(tput setaf 2)
 export YELLOW=$(tput setaf 3)
 export NORMAL=$(tput sgr0)
 
-#project_root=$(pwd)
+#tools
 export EVOSUITE_JAR="libs/evosuite-1.0.6.jar"
 export RANDOOP_JAR="libs/randoop-all-4.3.1.jar"
+export EVOSUITE_TIME=60
+export RANDOOP_TIME=60
+
+#resources
 export LITERALS="resources/literals.txt"
 
 origdir=$PWD
