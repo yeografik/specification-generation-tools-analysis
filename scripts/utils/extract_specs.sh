18319773b0885b8c2182af55f1c33ac82cf32295
Nacho Gonzalez <nachogonz2702@gmail.com>
2023-09-12 11:25:09 -0300

    Separated insertAssertion script into two scripts

---
+++ scripts/utils/extract_specs.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+#add conditions together with && and puts them in an array index
+function build_array_condition() {
+    local -n ref=$1
+    if [ "${ref[$3]}" == "" ]; then
+        ref[$3]="($2)"
+    else
+        ref[$3]="${ref[$3]} && ($2)"
+    fi
+}
+
+#add conditions together with &&
+function build_condition() {
+    if [ $3 -ne -1 ]; then  #there is a valid array index (!=-1)
+        build_array_condition $1 "$2" $3
+    else
+        local -n ref=$1
+        if [ "$ref" == "" ]; then
+            ref="($2)"
+        else
+            ref="$ref && ($2)"
+        fi
+    fi
+}
+
+#adds additional conditions to a condition with &&
+function join_condition() {
+    local -n final_cond=$2
+    if [ "$final_cond" == "" ]; then
+        final_cond="$1"
+    elif [ "$1" != "" ]; then
+        final_cond="$final_cond && $1"
+    fi
+}
+
+#adds additional conditions to a condition in an array with &&
+function join_post_conditions() {
+    local -n final_conds=$2
+    
+    for i in "${!final_conds[@]}";
+    do
+        if [ "${final_conds[$i]}" == "" ]; then
+            final_conds[$i]="$1"
+        elif [ "$1" != "" ]; then
+            final_conds[$i]="${final_conds[$i]} && ($1)"
+        fi
+    done
+}
+
+#get array index corresponding to the postcondition 
+function get_array_pos() {
+    local -n line_nums=$2
+    exit_num=$(echo "$1" | sed 's/.*[^0-9]//g')
+    ind=0
+    for line_num in "${line_nums[@]}"
+    do
+        if [ "$line_num" -eq "$exit_num" ]; then
+            echo $ind
+            return 0
+        fi
+        ((ind++))
+    done
+    echo -1
+}
+
+function check_index_error() {
+    if [ $1 -eq -1 ]; then
+        echo -e "\n\n${RED}Exit number could not be found in return lines${NORMAL}\n"
+        exit
+    fi
+}
+
+#read specs into variables to pass as arguments
+#precond="" from inset_assertion.sh
+#postconds=(return_lines[@].length) from inset_assertion.sh
+
+class_cond=""
+post_cond=""
+cond=""
+index=-1
+#loop through specs file and put them in an array
+while read -r line; do
+    if [[ $line == *"OBJECT"* ]]; then
+        cond='class_cond'
+    elif [[ $line == *"ENTER"* ]]; then
+        cond='pre_cond'
+    elif [[ "$line" =~ EXIT[0-9]+ ]]; then
+        cond='post_conds'
+        index=$(get_array_pos "$line" return_lines)
+        check_index_error $index
+    elif [[ $line == *"EXIT"* ]]; then
+        cond='post_cond'
+    elif [[ $line != *"===="* ]]; then
+        build_condition $cond "$line" $index
+    else #case of ===== line
+        index=-1
+    fi
+done <$specs
+
+#add class condition to pre and post conditions
+join_condition "$class_cond" pre_cond
+join_condition "$class_cond" post_cond
+join_post_conditions "$post_cond" post_conds
--- scripts/utils/insert_assertion.sh
+++ scripts/utils/insert_assertion.sh
@@ -1,70 +1,4 @@
 #!/bin/bash
-
-function build_array_condition() {
-    local -n ref=$1
-    if [ "${ref[$3]}" == "" ]; then
-        ref[$3]="($2)"
-    else
-        ref[$3]="${ref[$3]} && ($2)"
-    fi
-}
-
-function build_condition() {
-    if [ $3 -ne -1 ]; then  #index is not -1
-        build_array_condition $1 "$2" $3
-    else
-        local -n ref=$1
-        if [ "$ref" == "" ]; then
-            ref="($2)"
-        else
-            ref="$ref && ($2)"
-        fi
-    fi
-}
-
-function join_condition() {
-    local -n final_cond=$2
-    if [ "$final_cond" == "" ]; then
-        final_cond="$1"
-    elif [ "$1" != "" ]; then
-        final_cond="$final_cond && $1"
-    fi
-}
-
-function join_post_conditions() {
-    local -n final_conds=$2
-    
-    for i in "${!final_conds[@]}";
-    do
-        if [ "${final_conds[$i]}" == "" ]; then
-            final_conds[$i]="$1"
-        elif [ "$1" != "" ]; then
-            final_conds[$i]="${final_conds[$i]} && ($1)"
-        fi
-    done
-}
-
-function get_array_pos() {
-    local -n line_nums=$2
-    exit_num=$(echo "$1" | sed 's/.*[^0-9]//g')
-    ind=0
-    for num in "${line_nums[@]}"
-    do
-        if [ "$num" -eq "$exit_num" ]; then
-            echo $ind
-            return 0
-        fi
-        ((ind++))
-    done
-    echo -1
-}
-
-function check_index_error() {
-    if [ $1 -eq -1 ]; then
-        echo -e "\n\n${RED}Exit number could not be found in return lines${NORMAL}\n"
-    fi
-}
-
 #$1 java file, $2 class name $3 method name, $4 subject, $5 used tool
 
 specs="generated_specs/$4/$5/$5_inv.txt"
@@ -87,41 +21,13 @@ if [[ ! -f "/tmp/java_classes/specs/AssertionInserter.class" ]] && [[ ! -f "/tmp
     fi
 fi
 
-#get return line numbers
+#get line numbers where there is a return
 return_lines=()
 return_lines+=($(grep -n return $1 | grep -v @return | sed 's/^\([0-9]\+\):.*$/\1/'))
-# echo "array?: ${return_lines[@]}, array[0]: ${return_lines[0]}, array[1]: ${return_lines[1]}, array[2]: ${return_lines[2]}, length: ${#return_lines[@]}"
 post_conds=("${return_lines[@]/*/}")
-# echo "post_conds: ${post_conds[@]}fin"
-
-#read specs into variables to pass as arguments
-class_cond=""
 pre_cond=""
-post_cond=""
-cond=""
-index=-1
-while read -r line; do
-    if [[ $line == *"OBJECT"* ]]; then
-        cond='class_cond'
-    elif [[ $line == *"ENTER"* ]]; then
-        cond='pre_cond'
-    elif [[ "$line" =~ EXIT[0-9]+ ]]; then
-        cond='post_conds'
-        index=$(get_array_pos "$line" return_lines)
-        check_index_error $index
-    elif [[ $line == *"EXIT"* ]]; then
-        cond='post_cond'
-    elif [[ $line != *"===="* ]]; then
-        build_condition $cond "$line" $index
-    else #case of ===== line
-        index=-1
-    fi
-done <$specs
 
-#add class condition to pre and post conditions
-join_condition "$class_cond" pre_cond
-join_condition "$class_cond" post_cond
-join_post_conditions "$post_cond" post_conds
+source scripts/utils/extract_specs.sh
 
 #run AssertionInserter
 java -cp /tmp/:libs/javaparser-core-3.25.5-SNAPSHOT.jar:libs/javaparser-symbol-solver-core-3.25.5-SNAPSHOT.jar java_classes/specs/AssertionInserter $1 $2 $3 "$pre_cond" "${post_conds[@]}"
