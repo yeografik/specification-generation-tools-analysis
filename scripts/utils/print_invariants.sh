b4bf0aa1212cfb4b829338999337a5a9dff13f4a
Nacho Gonzalez <nachogonz2702@gmail.com>
2023-09-18 16:28:58 -0300

    Changed pattern of printInvariants to exclude methods with same name in substring

--- scripts/utils/print_invariants.sh
+++ scripts/utils/print_invariants.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$1 invariants path, $2 full class package, $3 objective method, $4 output dir
 
-java -cp $DAIKONDIR/daikon.jar daikon.PrintInvariants $1 --format Java --ppt-select-pattern="$2:::OBJECT|$2.$3" > $4
+java -cp $DAIKONDIR/daikon.jar daikon.PrintInvariants $1 --format Java --ppt-select-pattern="$2:::OBJECT|$2.$3\(" > $4
 
 #example usage ./scripts/utils/print_invariants.sh RegressionTestDriver.inv.gz DataStructures.StackAr push invariantes.txt
